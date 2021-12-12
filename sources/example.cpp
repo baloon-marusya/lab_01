a6393a59b2443c1692019c688fc6d935f078c1a6
melnikova.m <92054042+baloon-marusya@users.noreply.github.com>
2021-12-12 21:06:49 +0300

    Update example.cpp

--- sources/example.cpp
+++ sources/example.cpp
@@ -1,56 +1,56 @@
 //Copyright 2021 Maria Melnikova dandelion.m@mail.ru
 #include "example.hpp"
 
-void get_student(const json &j, Student &student) {
+void get_student(const nlohmann::json &j, Student &student) {
   student.name = get_name(j.at("name"));
   student.group = get_group(j.at("group"));
   student.avg = get_avg(j.at("avg"));
   student.debt = get_debt(j.at("debt"));
 }
 
-ifstream json_file(const string &path_to_file) {
+std::ifstream json_file(const std::string &path_to_file) {
   if (path_to_file.empty()) {
-    throw runtime_error{"The file path cannot be empty!"};
+    throw std::invalid_argument{"The file path cannot be empty!"};
   }
-  ifstream jf(path_to_file);
+  std::ifstream jf(path_to_file);
   if (!jf) {
-    throw runtime_error{"File: " + path_to_file + " does NOT exist!"};
+    throw std::out_of_range{"File: " + path_to_file + " does NOT exist!"};
   }
   return jf;
 }
 
-auto get_name(const json &j) -> string {
-  return j.get<string>();
+auto get_name(const nlohmann::json &j) -> std::string {
+  return j.get<std::string>();
 }
 
-auto get_debt(const json &j) -> any {
+auto get_debt(const nlohmann::json &j) -> std::any {
   if (j.is_null())
     return nullptr;
   else if (j.is_string())
-    return j.get<string>();
+    return j.get<std::string>();
   else
-    return j.get<vector<string> >();
+    return j.get<std::vector<std::string> >();
 }
 
-auto get_avg(const json &j) -> any {
+auto get_avg(const nlohmann::json &j) -> std::any {
   if (j.is_null())
     return nullptr;
   else if (j.is_string())
-    return j.get<string>();
+    return j.get<std::string>();
   else if (j.is_number_float())
     return j.get<double>();
   else
     return j.get<size_t>();
 }
 
-auto get_group(const json &j) -> any {
+auto get_group(const nlohmann::json &j) -> std::any {
   if (j.is_string())
-    return j.get<string>();
+    return j.get<std::string>();
   else
     return j.get<size_t>();
 }
 
-std::vector<Student> parse_file(json &data) {
+std::vector<Student> parse_file(nlohmann::json &data) {
   std::vector<Student> students;
   for (auto const &item : data.at("items")) {
     Student student;
@@ -60,51 +60,58 @@ std::vector<Student> parse_file(json &data) {
   return students;
 }
 
-void print_all_students(const vector<Student> &students, ostream &os) {
-  cout << "+" << setfill('-') << setw(25) << "+" << setw(10)
-       << "+" << setw(10) << "+" << setw(10) << "+" << endl;
-  cout << "|" << setfill(' ') << setw(24) << left << "name"
-       << "|" << setw(9) << left << "group"
-       << "|" << setw(9) << left << "avg"
-       << "|" << setw(9) << left << "debt" << "|"<< endl;
-  cout << "+" << setfill('-') << setw(25)
-       << right << "+" << setw(10)
-       << "+" << setw(10) << "+"
-       << setw(10) << right << "+" << endl;
+void print_all_students(const std::vector<Student> &students, std::ostream &os)
+{
+  std::cout << "+" << std::setfill('-') << std::setw(25) << "+" <<
+      std::setw(10) << "+" << std::setw(10) << "+" << std::setw(10) <<
+      "+" << std::endl;
+  std::cout << "|" << std::setfill(' ') << std::setw(24) << std::left <<
+      "name" << "|" << std::setw(9) << std::left << "group"
+            << "|" << std::setw(9) << std::left << "avg"
+            << "|" << std::setw(9) << std::left << "debt" << "|"<< std::endl;
+  std::cout << "+" << std::setfill('-') << std::setw(25)
+            << std::right << "+" << std::setw(10)
+            << "+" << std::setw(10) << "+"
+            << std::setw(10) << std::right << "+" << std::endl;
   for (auto const &student : students) {
     print_a_student(student, os);
   }
 }
-void print_a_student(const Student &student, ostream &os) {
-  os << "|" << setfill(' ') << setw(24)
-     << left << any_cast<string>(student.name)
-     << right << "|";
-  if (student.group.type() == typeid(string))
-    os << setw(9) << left << any_cast<string>(student.group)
-       << right << "|";
+void print_a_student(const Student &student, std::ostream &os) {
+  os << "|" << std::setfill(' ') << std::setw(24)
+     << std::left << std::any_cast<std::string>(student.name)
+     << std::right << "|";
+  if (student.group.type() == typeid(std::string))
+    os << std::setw(9) << std::left <<
+        std::any_cast<std::string>(student.group)
+       << std::right << "|";
   else
-    os << setw(9) << left <<  any_cast<size_t>(student.group)
+    os << std::setw(9) << std::left <<
+        std::any_cast<size_t>(student.group)
        << "|";
-  if ( student.avg.type() == typeid(string) )
-    os << setw(9) << left
-       << any_cast<string>(student.avg) << "|";
+  if ( student.avg.type() == typeid(std::string) )
+    os << std::setw(9) << std::left
+       << std::any_cast<std::string>(student.avg) << "|";
   else if (student.avg.type() == typeid(double))
-    os << setw(9) << left << any_cast<double>(student.avg)
+    os << std::setw(9) << std::left <<
+        std::any_cast<double>(student.avg)
        << "|";
-  else if (student.avg.type() == typeid(size_t))
-    os << setw(9) <<  left << any_cast<size_t>(student.avg)
+  else if (student.avg.type() == typeid(std::size_t))
+    os << std::setw(9) << std:: left <<
+        std::any_cast<size_t>(student.avg)
        << "|";
-  if (student.debt.type() == typeid(nullptr_t))
-    os << setw(9) << left << "null" << "|" << endl;
-  else if (student.debt.type() == typeid(string))
-    os << setw(9) << left << any_cast<string>(student.debt)
-       << "|" << endl;
-  else  os << setw(1) << left
-       << any_cast<vector<string>>(student.debt).size()
-       << setw(8) << left
-       << " items" << right << "|" << endl;
+  if (student.debt.type() == typeid(std::nullptr_t))
+    os << std::setw(9) << std::left << "null" << "|" << std::endl;
+  else if (student.debt.type() == typeid(std::string))
+    os << std::setw(9) << std::left <<
+        std::any_cast<std::string>(student.debt)
+       << "|" << std::endl;
+  else  os << std::setw(1) << std::left
+       << std::any_cast<std::vector<std::string>>(student.debt).size()
+       << std::setw(8) << std::left
+       << " items" << std::right << "|" << std::endl;
 
-  os << "+" << setfill('-') << setw(25) << right << "+"
-     << setw(10)
-     << "+" << setw(10) << "+" << setw(10) << "+" << endl;
+  os << "+" << std::setfill('-') << std::setw(25) << std::right << "+"
+     << std::setw(10)
+     << "+" << std::setw(10) << "+" << std::setw(10) << "+" << std::endl;
 }
